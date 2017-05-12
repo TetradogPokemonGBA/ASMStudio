0873db6970e09c5517c1f45d7e122e442e7747ad
Gabriel <tetradog@gmail.com>
2017-05-12 03:23:48 +0200

    subir archivos

---
+++ ASMStudio/ConfiguradorDeCaminos.xaml.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: tetra
+ * Fecha: 05/12/2017
+ * Hora: 03:05
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ASMStudio
+{
+	/// <summary>
+	/// Interaction logic for ConfiguradorDeCaminos.xaml
+	/// </summary>
+	public partial class ConfiguradorDeCaminos : UserControl
+	{
+		
+		UserControl[] pasos;
+		int pasoActual;
+		public event EventHandler Fin;
+		public ConfiguradorDeCaminos()
+		{
+			pasoActual=-1;
+			InitializeComponent();
+			//pongo los pasos y los configuro
+			PonSiguientePaso();
+		}
+		void BtnContinuar_Click(object sender, RoutedEventArgs e)
+		{
+			PonSiguientePaso();
+		}
+
+		void PonSiguientePaso()
+		{
+			pasoActual++;
+			if(pasoActual<pasos.Length){
+			gControlActual.Children.Clear();
+			gControlActual.Children.Add(pasos[pasoActual]);}else{
+				if(Fin!=null)
+					Fin(new object(),new EventArgs());
+			}
+		}
+	}
+}
---
+++ ASMStudio/Window1.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: tetra
+ * Fecha: 12/05/2017
+ * Hora: 2:52
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ASMStudio
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+	}
+}
