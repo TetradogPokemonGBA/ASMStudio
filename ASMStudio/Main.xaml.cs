6fe1d205b419eedccc3a11cd8a5e1d17bd4a0fbc
Gabriel <tetradog@gmail.com>
2017-05-15 17:09:17 +0200

    ahora carga el usuario de otra forma :)

--- ASMStudio/Main.xaml.cs
+++ ASMStudio/Main.xaml.cs
@@ -25,56 +25,75 @@ namespace ASMStudio
 	/// </summary>
 	public partial class Main : UserControl
 	{
+		RomHackingWebConnector.WahackConnector login;
 		public event EventHandler AbrirConfiguracion;
+		RowDefinition rowUser;
 		public Main()
 		{
 			InitializeComponent();
+			
+			login = new RomHackingWebConnector.WahackConnector();
+			login.Conectado += Conectado;
+			login.Desconectado += Desconectado;
+			login.Visibility = Visibility.Hidden;
+			//es el ultimo
+			gMain.Children.Add(login);
+			rowUser = gParent.RowDefinitions[2];
+			gParent.RowDefinitions.Remove(rowUser);
+			
+		}
+
+		void MiSalir_Click(object sender, RoutedEventArgs e)
+		{
+			Desconectado();
+		}
+		void Conectado(object sender = null, EventArgs e = null)
+		{
+			
+			if (login.EstaConectado) {
+				tbDeveloper.Text = "Desarrollador:\t" + login.User;
+				imgPerfil.SetImage(login.ImagenPerfil);
+				miLogin.Header = "Salir";
+				miLogin.Click -= MiLogin_Click;
+				miLogin.Click += MiSalir_Click;
+				gParent.RowDefinitions.Add(rowUser);
+				gUser.Visibility = Visibility.Visible;
+			}
+		}
+
+		void Desconectado(object sender = null, EventArgs e = null)
+		{
+			if (!miLogin.Header.ToString().Contains("Login")) {
+				
+				
+				login.Salir();
+				if (!login.EstaConectado) {
+					miLogin.Header = "Login";
+					login.Hide();
+					miLogin.Click += MiLogin_Click;
+					miLogin.Click -= MiSalir_Click;
+					tbDeveloper.Text = "";
+					imgPerfil.SetImage(new Bitmap(1, 1));
+					gUser.Visibility = Visibility.Hidden;
+					gParent.RowDefinitions.Remove(rowUser);
+				}
+			}
 		}
 		void MiConfiguracion_Click(object sender, RoutedEventArgs e)
 		{
-			if(AbrirConfiguracion!=null)
-				AbrirConfiguracion(new object(),new EventArgs());
+			if (AbrirConfiguracion != null)
+				AbrirConfiguracion(new object(), new EventArgs());
 		}
 		void MiSobre_Click(object sender, RoutedEventArgs e)
 		{
-			if(MessageBox.Show("Esta app esta desarrollada bajo licencia GPL GNU V3\nDesarrollador: Pikachu240\nCréditos:\nAndrea por el compilador.\nJavi4315 por sus conocimientos y ayuda.\nDedicado a Wahack una gran comunidad.\n\n¿Desa ver la pagina web donde esta el codigo fuente?","Sobre la aplicación",MessageBoxButton.YesNo,MessageBoxImage.Information)==MessageBoxResult.Yes)
+			if (MessageBox.Show("Esta app esta desarrollada bajo licencia GPL GNU V3\nDesarrollador: Pikachu240\nCréditos:\nAndrea por el compilador.\nJavi4315 por sus conocimientos y ayuda.\nDedicado a Wahack una gran comunidad.\n\n¿Desa ver la pagina web donde esta el codigo fuente?", "Sobre la aplicación", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
 				System.Diagnostics.Process.Start("https://github.com/TetradogPokemonGBA/ASMStudio");
 		}
 		void MiLogin_Click(object sender, RoutedEventArgs e)
 		{
-			LoginOnWahack login;
-			MenuItem miSalir;
-			if(miLogin.Header.ToString().Contains("Login"))
-			{
-				login=new LoginOnWahack();
-				//espero a que este descargada la web
-				
-				login.ShowDialog();
-				
-				if(login.IsConnected)
-				{
-					imgPerfil.Source=login.ImgPerfil.Source;
-					miLogin.Header=login.User;
-					miSalir=new MenuItem();
-					miSalir.Header="Salir";
-					miSalir.Click+=(s,m)=>
-					{	
-						LoginOnWahack logout=(LoginOnWahack)miLogin.Tag;
-						
-						logout.Salir();
-						if(!logout.IsConnected)
-						{
-							imgPerfil.SetImage(new Bitmap(1,1));
-							miLogin.Header="Login";
-							miLogin.Items.Clear();
-							miLogin.Click+=MiLogin_Click;
-						}
-					
-					};
-					miLogin.Tag=login;
-					miLogin.Items.Add(miSalir);
-					miLogin.Click-=MiLogin_Click;
-				}
+			login.Entrar();
+			if (login.EstaConectado) {
+				Conectado();
 			}
 		}
 	}
